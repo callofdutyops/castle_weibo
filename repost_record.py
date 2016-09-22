68ac1641923525e7952388c9473fc4f02294c843
callofdutyops <awsomekde@gmail.com>
2016-09-22 14:04:47 +0800

    add timepoing counter

---
+++ repost_record.py
@@ -0,0 +1,7 @@
+# Weibo class
+class RepostRecord:
+    def __init__(self, weibo_id, weibo_author_id, weibo_reauthor_id, weibo_timepoint):
+        self.weibo_id = weibo_id
+        self.weibo_author_id = weibo_author_id
+        self.weibo_reauthor_id = weibo_reauthor_id
+        self.weibo_timepoint = weibo_timepoint
