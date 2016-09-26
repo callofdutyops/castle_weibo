8a8489afdf19ca6657027a0553208fa3274fc492
callofdutyops <awsomekde@gmail.com>
2016-09-27 05:34:16 +0800

    complete breadth and deepth calc

--- data_importer.py
+++ data_importer.py
@@ -2,6 +2,7 @@
 
 # Simple test uses just the timepoing of weibo
 from collections import defaultdict
+
 import matplotlib.pyplot as plt
 import pandas as pd
 import networkx as nx
@@ -14,7 +15,7 @@ FILE_PATH = '/home/hp/Documents/DeepLearning/DataCastle/Weibo/Data/testRepostBef
 
 # Data importer
 def data_import(file_path):
-    origin = pd.read_csv(file_path, sep='\001', header=None)
+    origin = pd.read_table(file_path, sep='\001', header=None, usecols=[0, 1, 2, 3])
     repost_records = []
     weibo_ids = origin.iloc[:, 0].values
     weibo_author_ids = origin.iloc[:, 1].values
@@ -33,50 +34,75 @@ def calc_breadth(repost_records):
     return weibo_repostnum_dict
 
 
-def calc_deepth(repost_records, weibo_counter_dict):
+def calc_deepth(repost_records):
     weibo_repostdeepth_dict = defaultdict(list)
-    g = nx.Graph()
-    diameter = []
-    index = 0
-    for i in range(1, 3001):
-        weibo_id = 'testWeibo' + str(i)
-        for j in range(len(weibo_counter_dict[weibo_id])):
-            g.add_edge(repost_records[index].weibo_author_id, repost_records[index].weibo_reauthor_id)
-            graphs = list(nx.connected_component_subgraphs(g))
-            for k in range(len(graphs)):
-                diameter.append(nx.diameter(graphs[k]))
-            weibo_repostdeepth_dict[repost_records[index].weibo_id].append((repost_records[index].weibo_timepoint,
-                                                                            max(diameter)))
-            index += 1
-        g.clear()
-        diameter = []
+    i = 0
+    g = nx.DiGraph()
+    g.add_node(0)
+    weibo_start_id = repost_records[i].weibo_id
+    weibo_start_author_id = repost_records[i].weibo_author_id
+    weibo_author_id = repost_records[i].weibo_author_id
+    weibo_reauthor_id = repost_records[i].weibo_reauthor_id
+    g.add_edge(0, weibo_author_id)
+    g.add_edge(weibo_author_id, weibo_reauthor_id)
+    weibo_repostdeepth_dict[repost_records[i].weibo_id].append((repost_records[i].weibo_timepoint,
+                                                                int(nx.eccentricity(g, v=0)) - 1))
+    i += 1
+    while i != len(repost_records) - 1:
+        weibo_cur_id = repost_records[i].weibo_id
+        weibo_author_id = repost_records[i].weibo_author_id
+        weibo_reauthor_id = repost_records[i].weibo_reauthor_id
+        if weibo_start_id == weibo_cur_id:
+            if weibo_author_id not in g.nodes():
+                g.add_edge(weibo_start_author_id, weibo_author_id)
+            if weibo_reauthor_id not in g.nodes():
+                g.add_edge(weibo_author_id, weibo_reauthor_id)
+            weibo_repostdeepth_dict[repost_records[i].weibo_id].append((repost_records[i].weibo_timepoint,
+                                                                        int(nx.eccentricity(g, v=0)) - 1))
+        else:
+            g.clear()
+            g.add_node(0)
+            weibo_start_id = repost_records[i].weibo_id
+            weibo_start_author_id = repost_records[i].weibo_author_id
+            weibo_author_id = repost_records[i].weibo_author_id
+            weibo_reauthor_id = repost_records[i].weibo_reauthor_id
+            g.add_edge(0, weibo_author_id)
+            g.add_edge(weibo_author_id, weibo_reauthor_id)
+        i += 1
     return weibo_repostdeepth_dict
 
 
-def main():
-    repost_records = data_import(FILE_PATH)
-    weibo_counter_dict = calc_breadth(repost_records)
+def save_plot_breadth(weibo_counter_dict):
     with open('weibo_counter_dict.pickle', 'wb') as handle:
         pickle.dump(weibo_counter_dict, handle, protocol=pickle.HIGHEST_PROTOCOL)
-    for i in range(1, len(weibo_counter_dict) + 1):
+    for i in range(1, 3001):
         weibo_id = 'testWeibo' + str(i)
         axis_x = weibo_counter_dict[weibo_id]
         axis_y = [x + 1 for x in range(len(weibo_counter_dict[weibo_id]))]
         plt.plot(axis_x, axis_y)
     plt.savefig('weibo_counter.png')
-    weibo_repostdeepth_dict = calc_deepth(repost_records, weibo_counter_dict)
+
+
+def save_plot_deepth(weibo_repostdeepth_dict):
     with open('weibo_repostdeepth_dict.pickle', 'wb') as handle:
         pickle.dump(weibo_repostdeepth_dict, handle, protocol=pickle.HIGHEST_PROTOCOL)
-    for i in range(1, len(weibo_repostdeepth_dict) + 1):
+    for i in range(1, 3001):
         weibo_id = 'testWeibo' + str(i)
-        axis_x2 = []
-        axis_y2 = []
+        axis_x = []
+        axis_y = []
         for x, y in weibo_repostdeepth_dict[weibo_id]:
-            axis_x2.append(x)
-            axis_y2.append(y)
-            plt.plot(axis_x2, axis_y2)
+            axis_x.append(x)
+            axis_y.append(y)
+        plt.plot(axis_x, axis_y)
     plt.savefig('weibo_repostdeepth.png')
 
+
+def main():
+    repost_records = data_import(FILE_PATH)
+    weibo_counter_dict = calc_breadth(repost_records)
+    save_plot_breadth(weibo_counter_dict)
+    weibo_repostdeepth_dict = calc_deepth(repost_records)
+    save_plot_deepth(weibo_repostdeepth_dict)
 
 if __name__ == '__main__':
     main()
